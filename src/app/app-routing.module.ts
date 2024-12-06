75bcb95b19cb9afcc66b9e4a03cdab945033a9a4
Bangar Mahammed Ghouse <111179871+MDGHOUSE18@users.noreply.github.com>
2024-12-06 15:30:27 +0530

    commit changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RegisterComponent } from './Components/register/register.component';
 import { ForgotpasswordComponent } from './Components/forgotpassword/forgotpassword.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'forgotpassword',component:ForgotpasswordComponent}
 ];
